ad32880d81f5ba32a4b2e6eaa2bc2eaa6aab2ef4
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-07-07 23:12:48 -0400

    pkgs: Patch SDL_ttf

--- system-configuration/overlay.nix
+++ system-configuration/overlay.nix
@@ -3,6 +3,11 @@
 {
   nixpkgs.overlays = [
     (self: super: {
+      SDL_ttf = super.SDL_ttf.overrideAttrs({ patches ? [], ... }: {
+        patches = patches ++ [
+          ../pkgs/SDL_ttf/0001-Backport-Fixed-bug-3762-Can-t-render-characters-with.patch
+        ];
+      });
     })
   ];
 }
