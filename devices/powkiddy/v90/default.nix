f4b280e0f948827a856c79760da80daa43052c7c
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-07-10 05:59:26 -0400

    powkiddy/v90: somewhat implement choosing provenance
    This is for development purposes, not for end-users.

--- devices/powkiddy/v90/default.nix
+++ devices/powkiddy/v90/default.nix
@@ -1,7 +1,12 @@
 { config, lib, pkgs, ... }:
 
 let
-  u-boot = pkgs.callPackage ./u-boot { };
+  mainline = {
+    provenance = "mainline";
+    u-boot = pkgs.callPackage ./mainline/u-boot { };
+    kernel = pkgs.callPackage ./mainline/kernel { };
+  };
+  selected = mainline;
 in
 {
   device = {
@@ -11,11 +16,13 @@ in
     ];
     config.allwinner = {
       embedFirmware = true;
-      firmwarePartition = u-boot + "/u-boot-sunxi-with-spl.bin";
+      firmwarePartition = selected.u-boot + "/u-boot-sunxi-with-spl.bin";
 
       # Unclear whether this is an allwinner-wide bug, or f1c100s only...
       # But FEL-booted systems don't see the environment set by the FEL tool.
-      fel-firmware = (u-boot.overrideAttrs({preConfigure ? "", ...}: {
+      # NOTE: it's assumed that we'll use FEL only with mainline U-Boot...
+      #       this is not the expected way to run anything really.
+      fel-firmware = (mainline.u-boot.overrideAttrs({preConfigure ? "", ...}: {
         preConfigure = preConfigure + ''
           cat <<EOF >> configs/powkiddy_v90_defconfig
           CONFIG_BOOTCOMMAND="source \''${scriptaddr}"
@@ -30,7 +37,7 @@ in
     cpu = "allwinner-f1c100s";
   };
 
-  wip.kernel.package = pkgs.callPackage ./kernel {};
+  wip.kernel.package = selected.kernel;
   wip.kernel.defconfig = "miyoo_defconfig";
 
   wip.kernel.structuredConfig =
@@ -41,10 +48,10 @@ in
     lib.mkMerge [
       {
         # Not available here
-        SERIAL_AMBA_PL011 = no;
-        SERIAL_AMBA_PL011_CONSOLE = no;
-
-        MEMFD_CREATE = yes;
+        SERIAL_AMBA_PL011 = lib.mkForce no;
+        SERIAL_AMBA_PL011_CONSOLE = lib.mkForce no;
+        MEMFD_CREATE = lib.mkForce (if selected.provenance == "mainline" then yes else no);
+        LOCALVERSION = freeform ''""'';
 
         # ??
         VT_CONSOLE = option no;
@@ -63,15 +70,20 @@ in
 
       }
 
+      /*
+      # TODO: mainline USB support is iffy.
+      # It will work only if booted from FEL
+      # I assume FEL initializes USB, while no other component knows how to.
       {
         USB_ROLE_SWITCH = yes;
       }
+      */
 
-      {
+      (lib.mkIf (selected.provenance == "mainline") {
         DRM = yes;
         DRM_SUN4I = yes;
         DRM_PANEL_SIMPLE = yes;
-      }
+      })
     ]
   ;
 
