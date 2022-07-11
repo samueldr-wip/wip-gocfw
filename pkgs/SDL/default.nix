1a6c3f9fcdee449236505e064376c077f96e1aff
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-07-11 17:54:23 -0400

    SDL: disable things

--- pkgs/SDL/default.nix
+++ pkgs/SDL/default.nix
@@ -1,6 +1,33 @@
 { SDL }:
 
-SDL.override {
+(SDL.overrideAttrs({ configureFlags, ... }: {
+  # Ensures nothing unneeded is built
+  configureFlags = [
+    "--enable-cdrom=no"
+
+    "--enable-oss=no"
+    "--enable-esd=no"
+    "--enable-arts=no"
+    "--enable-nas=no"
+    "--enable-diskaudio=no"
+    #"--enable-dummyaudio=no"
+
+    "--enable-video-x11=no"
+    "--enable-dga=no"
+    "--enable-video-dga=no"
+    "--enable-video-directfb=no"
+    "--enable-video-svga=no"
+    "--enable-video-vgl=no"
+    "--enable-video-wscons=no"
+    "--enable-video-aalib=no"
+    "--enable-video-caca=no"
+    "--enable-video-qtopia=no"
+    "--enable-video-picogui=no"
+    #"--enable-video-dummy=no"
+    "--enable-video-opengl=no"
+  ];
+}))
+.override {
   pulseaudioSupport = false;
   libGLSupported = false;
   openglSupport = false;
