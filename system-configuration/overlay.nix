4a751766c4e5f48c960786fa7726792fbf9c262b
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-07-12 21:18:37 -0400

    SDL_ttf: handle the whole build locally
    I don't want to send an unstable build upstream, so eh, let's handle the
    whole derivation locally.

--- system-configuration/overlay.nix
+++ system-configuration/overlay.nix
@@ -7,7 +7,7 @@
         hello = self.callPackage ../pkgs/hello { };
       };
       SDL = self.callPackage ../pkgs/SDL { inherit (super) SDL; };
-      SDL_ttf = self.callPackage ../pkgs/SDL_ttf { inherit (super) SDL_ttf; };
+      SDL_ttf = self.callPackage ../pkgs/SDL_ttf { };
       luajit = self.callPackage ../pkgs/luajit { inherit (super) luajit; };
     })
   ];
