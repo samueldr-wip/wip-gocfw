1e4dc9ef4d3736364fff8edd781ee8bc15dcc3d2
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-07-17 21:12:39 -0400

    powkiddy/v90: Fixups for stub usage
    Verified to boot correctly on vendor kernel; it looks like my "hello"
    app fails on mainline-based kernel though.

--- devices/powkiddy/v90/default.nix
+++ devices/powkiddy/v90/default.nix
@@ -1,6 +1,11 @@
 { config, lib, pkgs, ... }:
 
 let
+  inherit (config.device) nameForDerivation;
+  inherit (lib)
+    mkIf
+    mkMerge
+  ;
   mainline = {
     provenance = "mainline";
     u-boot = pkgs.callPackage ./mainline/u-boot { };
@@ -13,125 +18,141 @@ let
   };
   #selected = mainline;
   selected = vendor;
+  writeScriptDir = name: text: pkgs.writeTextFile {
+    inherit name text;
+    executable = true;
+    destination = "${name}";
+  };
 in
 {
-  device = {
-    name = "powkiddy/v90";
-    dtbFiles = [
-      "suniv-f1c100s-powkiddy-v90.dtb"
-    ];
-    config.allwinner = {
-      embedFirmware = true;
-      firmwarePartition = selected.u-boot + "/u-boot-sunxi-with-spl.bin";
-
-      # Unclear whether this is an allwinner-wide bug, or f1c100s only...
-      # But FEL-booted systems don't see the environment set by the FEL tool.
-      # NOTE: it's assumed that we'll use FEL only with mainline U-Boot...
-      #       this is not the expected way to run anything really.
-      fel-firmware = (mainline.u-boot.overrideAttrs({preConfigure ? "", ...}: {
-        preConfigure = preConfigure + ''
-          cat <<EOF >> configs/powkiddy_v90_defconfig
-          CONFIG_BOOTCOMMAND="source \''${scriptaddr}"
-          EOF
-          cat configs/powkiddy_v90_defconfig
-        '';
-      })) + "/u-boot-sunxi-with-spl.bin";
-    };
-  };
-
-  hardware = {
-    cpu = "allwinner-f1c100s";
-  };
-
-  wip.kernel.package = selected.kernel;
-  wip.kernel.defconfig = "miyoo_defconfig";
-
-  wip.kernel.isModular = true;
-  wip.kernel.structuredConfig =
-    with lib.kernel;
-    let
-      inherit (config.wip.kernel) features;
-    in
-    lib.mkMerge [
-      {
-        # Not available here
-        SERIAL_AMBA_PL011 = lib.mkForce no;
-        SERIAL_AMBA_PL011_CONSOLE = lib.mkForce no;
-        MEMFD_CREATE = lib.mkForce (if selected.provenance == "mainline" then yes else no);
-        LOCALVERSION = freeform ''""'';
-
-        # ??
-        VT_CONSOLE = option no;
-
-        SERIAL_EARLYCON = yes;
-        SERIAL_8250 = yes;
-        SERIAL_8250_DEPRECATED_OPTIONS = yes;
-        SERIAL_8250_CONSOLE = yes;
-        SERIAL_8250_NR_UARTS = freeform "8";
-        SERIAL_8250_RUNTIME_UARTS = freeform "8";
-        SERIAL_8250_FSL = yes;
-        SERIAL_8250_DW = yes;
-        SERIAL_OF_PLATFORM = yes;
-        SERIAL_CORE = yes;
-        SERIAL_CORE_CONSOLE = yes;
-
-      }
-
-      /*
-      # TODO: mainline USB support is iffy.
-      # It will work only if booted from FEL
-      # I assume FEL initializes USB, while no other component knows how to.
-      {
-        USB_ROLE_SWITCH = yes;
-      }
-      */
-
-      (lib.mkIf (selected.provenance == "mainline") {
-        DRM = yes;
-        DRM_SUN4I = yes;
-        DRM_PANEL_SIMPLE = yes;
-      })
-    ]
-  ;
-
-  # xz fails to uncompress due to lack of memory
-  wip.stage-1.compression = lib.mkForce "gzip";
-
-  boot.cmdline = lib.mkMerge [
-    [
-      "console=tty0"
-      "console=ttyS1,115200"
-      "panic=5"
-    ]
-    (lib.mkIf (selected.provenance == "vendor") [
-      "miyoo.miyoo_snd=1"
-      "miyoo_kbd.miyoo_layout=1"
-      "miyoo_kbd.miyoo_ver=2"
-    ])
+  imports = [
+    ./disk-image.nix
   ];
 
-  # TODO: if we want to build a common miyoo kernel, and load modules, this
-  #       could be done a bit like so:
-  #examples.hello-wip-games-os.rootfs.contents = {
-  #  "lib/modules" = pkgs.runCommandNoCC "hello-wip-games-os--initramfs-lib-modules" {} ''
-  #    mkdir -p $out/lib
-  #    cp -prf ${config.build.kernel}/lib/modules $out/lib/modules
-  #  '';
-  #};
-  #examples.hello-wip-games-os.extraUtils.packages = [
-  #  # This script is mostly verbatim from the vendor image.
-  #  (pkgs.writeScriptBin "vendor-kernel-modules" ''
-  #    #!/bin/sh
-  #
-  #    echo "::"
-  #    set -x
-  #    modprobe st7789sfb
-  #  '')
-  #];
+  config = mkMerge [
+    {
+      device = {
+        name = "powkiddy/v90";
+        dtbFiles = [
+          "suniv-f1c100s-powkiddy-v90.dtb"
+        ];
+        config.allwinner = {
+          embedFirmware = true;
+          firmwarePartition = selected.u-boot + "/u-boot-sunxi-with-spl.bin";
 
-  nixpkgs.overlays = [
-    (self: super: {
-      SDL = self.callPackage ./pkgs/SDL { inherit (super) SDL; };
+          # Unclear whether this is an allwinner-wide bug, or f1c100s only...
+          # But FEL-booted systems don't see the environment set by the FEL tool.
+          # NOTE: it's assumed that we'll use FEL only with mainline U-Boot...
+          #       this is not the expected way to run anything really.
+          fel-firmware = (mainline.u-boot.overrideAttrs({preConfigure ? "", ...}: {
+            preConfigure = preConfigure + ''
+              cat <<EOF >> configs/powkiddy_v90_defconfig
+              CONFIG_BOOTCOMMAND="source \''${scriptaddr}"
+              EOF
+              cat configs/powkiddy_v90_defconfig
+            '';
+          })) + "/u-boot-sunxi-with-spl.bin";
+        };
+      };
+
+      hardware = {
+        cpu = "allwinner-f1c100s";
+      };
+
+      # Target device uses initramfs.
+      wip.stage-1.enable = true;
+      wip.stage-1.output.initramfs = config.games-os.stub.filesystem.output;
+
+      wip.kernel.package = selected.kernel;
+      wip.kernel.defconfig = "miyoo_defconfig";
+
+      wip.kernel.isModular = true;
+      wip.kernel.structuredConfig =
+        with lib.kernel;
+        let
+          inherit (config.wip.kernel) features;
+        in
+        mkMerge [
+          {
+            # Not available here
+            SERIAL_AMBA_PL011 = lib.mkForce no;
+            SERIAL_AMBA_PL011_CONSOLE = lib.mkForce no;
+            MEMFD_CREATE = lib.mkForce (if selected.provenance == "mainline" then yes else no);
+            LOCALVERSION = freeform ''""'';
+
+            # ??
+            VT_CONSOLE = option no;
+
+            SERIAL_EARLYCON = yes;
+            SERIAL_8250 = yes;
+            SERIAL_8250_DEPRECATED_OPTIONS = yes;
+            SERIAL_8250_CONSOLE = yes;
+            SERIAL_8250_NR_UARTS = freeform "8";
+            SERIAL_8250_RUNTIME_UARTS = freeform "8";
+            SERIAL_8250_FSL = yes;
+            SERIAL_8250_DW = yes;
+            SERIAL_OF_PLATFORM = yes;
+            SERIAL_CORE = yes;
+            SERIAL_CORE_CONSOLE = yes;
+          }
+
+          /*
+          # TODO: mainline USB support is iffy.
+          # It will work only if booted from FEL
+          # I assume FEL initializes USB, while no other component knows how to.
+          {
+            USB_ROLE_SWITCH = yes;
+          }
+          */
+
+          (mkIf (selected.provenance == "mainline") {
+            DRM = yes;
+            DRM_SUN4I = yes;
+            DRM_PANEL_SIMPLE = yes;
+          })
+        ]
+      ;
+
+      # xz fails to uncompress due to lack of memory
+      wip.stage-1.compression = lib.mkForce "gzip";
+
+      boot.cmdline = mkMerge [
+        [
+          "console=ttyS1,115200"
+          "console=tty0"
+          "panic=5"
+        ]
+        (mkIf (selected.provenance == "vendor") [
+          "miyoo.miyoo_snd=1"
+          "miyoo_kbd.miyoo_layout=1"
+          "miyoo_kbd.miyoo_ver=2"
+        ])
+      ];
+
+      games-os.stub.userdataPartition = "/dev/mmcblk0p2";
+    }
+
+    (mkIf (selected.provenance == "vendor") {
+      games-os.stub.filesystem.contents = {
+        "lib/modules" = pkgs.runCommandNoCC "${nameForDerivation}-lib-modules" {} ''
+          mkdir -p $out/lib
+          cp -prf ${config.build.kernel}/lib/modules $out/lib/modules
+        '';
+
+        "/etc/init.d/70-debug" = writeScriptDir "/etc/init.d/70-debug" ''
+          #!/bin/sh
+          
+          echo ":: Inserting kernel modules"
+          PS4=" $ "; set -x
+          modprobe st7789sfb
+        '';
+      };
+
+      nixpkgs.overlays = [
+        (self: super: {
+          SDL = self.callPackage ./pkgs/SDL { inherit (super) SDL; };
+        })
+      ];
     })
   ];
 }
---
+++ devices/powkiddy/v90/disk-image.nix
@@ -0,0 +1,115 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib)
+    concatStringsSep
+    optionalString
+    mapAttrsToList
+    mkOption
+    types
+  ;
+  inherit (pkgs)
+    stdenv
+  ;
+  inherit (stdenv) hostPlatform isAarch64;
+  kernel = config.wip.kernel.output;
+  inherit (kernel) target;
+  inherit (config.wip.stage-1.output) initramfs;
+  inherit (config.device) dtbFiles nameForDerivation;
+  cfg = config.device.config.allwinner;
+
+  bootscr = config.wip.u-boot.lib.mkScript "boot.scr" ''
+    echo "Booting gofcw stub..."
+    setenv bootargs ${concatStringsSep " " config.boot.cmdline}
+
+    if test "$mmc_bootdev" != ""; then
+      devtype="mmc"
+    else
+      exit
+    fi
+
+    if test "$devtype" = "mmc"; then
+      devnum="$mmc_bootdev"
+    fi
+
+    bootpart="2"
+
+    if load ''${devtype} ''${devnum}:''${bootpart} ''${kernel_addr_r} /.boot/kernel.img; then
+      setenv boot_type boot
+    else
+      echo "!!! Failed to load kernel !!!"
+      exit
+    fi
+
+    if load ''${devtype} ''${devnum}:''${bootpart} ''${fdt_addr_r} /.boot/dtbs/''${fdtfile}; then
+      fdt addr ''${fdt_addr_r}
+      fdt resize
+    fi
+
+    load ''${devtype} ''${devnum}:''${bootpart} ''${ramdisk_addr_r} /.boot/initramfs.img
+    setenv ramdisk_size ''${filesize}
+
+    bootz ''${kernel_addr_r} ''${ramdisk_addr_r}:''${ramdisk_size} ''${fdt_addr_r};
+  '';
+
+  rootfs = config.build.TEMProotfs;
+in
+{
+  build.disk-image = (pkgs.celun.image-builder.evaluateDiskImage {
+    config =
+      { config, ... }:
+
+      let inherit (config) helpers; in
+      {
+        name = "${nameForDerivation}-disk-image";
+        partitioningScheme = "gpt";
+        gpt.partitionEntriesCount = 48;
+
+        partitions = [
+          (lib.mkIf cfg.embedFirmware {
+            name = "firmware";
+            partitionLabel = "$FIRMWARE";
+            partitionType = "67401509-72E7-4628-B1AF-EDD128E4316A";
+            offset = 16 * 512 /* sectors */; # 8MiB from the start of the disk
+            length = helpers.size.MiB 4;
+            raw = cfg.firmwarePartition;
+          })
+
+          {
+            name = "boot-partition";
+            partitionLabel = "userdata";
+            partitionType = "8DA63339-0007-60C0-C436-083AC8230908";
+            bootable = true;
+            filesystem = {
+              filesystem = "ext4";
+              label = "userdata";
+              extraPadding = helpers.size.MiB 32;
+              populateCommands = ''
+                cp ${bootscr} boot.scr
+
+                mkdir -p .boot
+                cp ${initramfs} .boot/initramfs.img
+                cp ${kernel}/${target} .boot/kernel.img
+
+                # There might not be any DTBs to install; on ARM the DTB files
+                # are built only if the proper ARCH_VENDOR config is set.
+                if [ -e ${kernel}/dtbs ]; then
+                  (
+                  shopt -s globstar
+                  mkdir .boot/dtbs/
+                  cp -fvr ${kernel}/dtbs/**/*.dtb .boot/dtbs
+                  )
+                else
+                  echo "Warning: no dtbs built on hostPlatform with DTB"
+                fi
+
+                mkdir -p system
+                cp ${rootfs} system/rootfs.img
+              '';
+            };
+          }
+        ];
+      }
+    ;
+  }).config.output;
+}
