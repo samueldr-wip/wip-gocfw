9053ddd0eca0bd7ccf22e208a515f17f66159af1
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-11-14 01:15:32 -0500

    pkgs/hello: WIP alternative implementation

--- pkgs/hello/default.nix
+++ pkgs/hello/default.nix
@@ -1,37 +1,32 @@
 { stdenv
-, runtimeShell
-, luajit
+
+, meson
+, ninja
+, pkg-config
+, vala
+
 , SDL
-, SDL_ttf
-, SDL_image
+, cairo
+, glib
 }:
 
 stdenv.mkDerivation {
   pname = "games-os-hello";
-  version = "20220707";
+  version = "2022-11-09";
 
   # XXX
-  src = builtins.fetchGit /Users/samuel/Network/dashdingo.local/tmp/launcher/sdl-luajit-launcher;
+  src = builtins.fetchGit /Users/samuel/Network/dashdingo.local/tmp/launcher/vala/hello-sdl;
 
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
+  buildInputs = [
+    SDL
+    cairo
+    glib
+  ];
 
-    cat >> $out/bin/hello <<EOF
-    #!${runtimeShell}
-    export LD_LIBRARY_PATH
-    LD_LIBRARY_PATH+=:${SDL.out}/lib:${SDL_ttf.out}/lib:${SDL_image.out}/lib
-    export APP_PATH
-    APP_PATH="\''${BASH_SOURCE[0]%/*}/../share/games-os-hello"
-    exec ${luajit}/bin/luajit "\$APP_PATH/hello.lua" "\$@"
-    EOF
-    chmod +x $out/bin/hello
-
-    mkdir -p $out/share/games-os-hello
-    cp -vt $out/share/games-os-hello/ *.lua
-    cp -vr resources $out/share/games-os-hello/resources
-
-    runHook postInstall
-  '';
-
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+    vala
+  ];
 }
