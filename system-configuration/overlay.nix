ce670a1bbeba802427e80aa61ab34132c9e6be51
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-11-14 01:20:22 -0500

    SDL_ttf: drop, we're going to cairo!

--- system-configuration/overlay.nix
+++ system-configuration/overlay.nix
@@ -8,7 +8,6 @@
         mkDotApp = self.callPackage ../pkgs/mkDotApp { };
       };
       SDL = self.callPackage ../pkgs/SDL { inherit (super) SDL; };
-      SDL_ttf = self.callPackage ../pkgs/SDL_ttf { };
       luajit = self.callPackage ../pkgs/luajit { inherit (super) luajit; };
     })
   ];
