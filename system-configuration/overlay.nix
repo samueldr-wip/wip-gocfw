f7ebea5fdfbc854d2cfe5ab77b4b100bd856194f
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-01-07 23:18:46 -0500

    overlay: Add workaround for systemd on MIPS

--- system-configuration/overlay.nix
+++ system-configuration/overlay.nix
@@ -1,16 +1,34 @@
 { config, lib, ... }:
 
+let
+  inherit (lib)
+    mkIf
+    mkMerge
+  ;
+in
 {
-  nixpkgs.overlays = [
-    (self: super: {
-      games-os = {
-        hello = self.callPackage ../pkgs/hello { };
-        mkDotApp = self.callPackage ../pkgs/mkDotApp { };
-        ubiboot = self.callPackage ../pkgs/ubiboot { };
-      };
-      SDL = self.callPackage ../pkgs/SDL { inherit (super) SDL; };
-      SDL_ttf = self.callPackage ../pkgs/SDL_ttf { };
-      luajit = self.callPackage ../pkgs/luajit { inherit (super) luajit; };
-    })
+  nixpkgs.overlays = mkMerge [
+    [
+      (self: super: {
+        games-os = {
+          hello = self.callPackage ../pkgs/hello { };
+          mkDotApp = self.callPackage ../pkgs/mkDotApp { };
+          ubiboot = self.callPackage ../pkgs/ubiboot { };
+        };
+        SDL = self.callPackage ../pkgs/SDL { inherit (super) SDL; };
+        SDL_ttf = self.callPackage ../pkgs/SDL_ttf { };
+        luajit = self.callPackage ../pkgs/luajit { inherit (super) luajit; };
+      })
+    ]
+
+    # MIPS specific workarounds
+    (mkIf config.nixpkgs.crossSystem.isMips [
+      (self: super: {
+        systemd = super.systemd.override({
+          # On mips, clang 11, a dep for withLibBPF, doesn't build...
+          withLibBPF = false;
+        });
+      })
+    ])
   ];
 }
