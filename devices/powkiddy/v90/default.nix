6d9d35a22e74a062b19a5da1ef5641943759c485
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-01-15 21:31:35 -0500

    powkiddy/v90: Add device information

--- devices/powkiddy/v90/default.nix
+++ devices/powkiddy/v90/default.nix
@@ -59,6 +59,23 @@ in
         cpu = "allwinner-f1c100s";
       };
 
+      wip.gocfw.device-information = {
+        display = { width = 320; height = 240; };
+        input.buttons = {
+          menu.available = true;
+          reset.available = false;
+          power.type = "hard";
+          volume.type = "wheel-hard";
+        };
+        storage = {
+          built-in.type = "none";
+          external = {
+            available = true;
+            bootable = true;
+          };
+        };
+      };
+
       # Target device uses initramfs.
       wip.stage-1.enable = true;
       wip.stage-1.cpio = config.games-os.stub.filesystem.output;
